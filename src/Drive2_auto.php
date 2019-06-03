0a0ce591b0bfc8c844c104577d28e88d9c05d930
jomborus <jomborus@gmail.com>
2019-06-03 22:00:08 +0300

    добавлен поиск по машинам

--- src/Drive2_auto.php
+++ src/Drive2_auto.php
@@ -1,30 +1,88 @@
 <?php
 
-$A = new Drive2_auto;
-if(isset($_POST['proces']) && $_POST['proces'] == 1)
-	$A->GetBase();
-if(isset($_POST['proces']) && $_POST['proces'] == 2)
-	$A->GetInfo();
+$msqli = file_get_contents('mysql.ini');
+$arr_msql = explode('
+', $msqli);
+foreach ($arr_msql as $key => $value) {
+	$arr_swap = explode('=', $value);
+	$arr_msql[$key] = $arr_swap[1];
+}
 
-print '<form action="" method="post">
-			<p><select name="proces">
-			<option selected disabled>Чего вы хотите от этой программы?</option>
+
+$A = new Drive2_auto;
+
+$A->Choice_Base($arr_msql[0], $arr_msql[1], $arr_msql[2], $arr_msql[3], 'parser');
+
+if(isset($_POST['seach_marks']))
+	$proces_marks = $_POST['seach_marks'];
+else $proces_marks = 'm';
+if(isset($_POST['seach_modells']))
+	$proces_modells = $_POST['seach_modells'];
+else $proces_modells = 'm';
+
+if(isset($_POST['proces_all']) && $_POST['proces_all'] == 1){
+	$A->GetBase($proces_marks, $proces_modells);
+}
+if(isset($_POST['proces_all']) && $_POST['proces_all'] == 2){
+	$A->GetInfo($proces_marks, $proces_modells);
+}
+
+$html_str = file_get_contents("https://www.drive2.ru/cars/?all"); 
+
+$marks = array();
+
+preg_match_all('#<a[^>]+?class\s*?=\s*?["\']c-link c-link--text["\'][^>]+?href=["\'](.+?)["\'][^>]*?>(.+?)</a>#su', $html_str, $marks);
+$count_marks = count($marks[1]);
+
+$result = '<form action="" method="post">
+			<p><select name="proces_all">
+			<option selected value="m">Чего вы хотите от этой программы?</option>
 			<option value="1">хотите вывести информацию из базы данных?</option>
 			<option value="2">хотите спарсить информацию с сайта?</option>
-			</select></p>
-			<p><input type="submit" value="Отправить запрос"></p>
+			</select></p>';
+
+$result .= '<p><select name="seach_marks"><option selected value="m">Какая марка вас интересует?</option>';
+for ($key_mark = 2; $key_mark < $count_marks; ++$key_mark){
+	$result .= '<option value="'.$marks[1][$key_mark].'">'.$marks[2][$key_mark].'</option>';
+}
+$result .= '</select>';
+
+if(isset($_POST['seach_marks'])){
+	
+	$html_str_cars = file_get_contents('https://www.drive2.ru'.$proces_marks);
+	$modells = array();
+	preg_match_all('#<a[^>]+?class\s*?=\s*?["\']c-link c-link--text["\'][^>]+?href=["\'](.+?)["\'][^>]*?>(.+?)</a>#su', $html_str_cars, $modells);
+	$count_modells = count($modells[1]);
+	
+	$result .= '<p>'.$proces_marks.'</p>';
+	$result .= '<p><select name="seach_modells"><option selected value="m">Какая модель вас интересует?</option>';
+	for($key_modell = 1; $key_modell < $count_modells; ++$key_modell){
+		$result .= '<option value="'.$modells[1][$key_modell].'">'.$modells[2][$key_modell].'</option>';
+	}
+	$result .= '</select>';
+
+}
+$result .=	'	<p><input type="submit" value="Отправить запрос"></p>
 			</form>';
 
+print $result;
+
 class Drive2_auto { 
+	private $__host = 'localhost'; 
+	private $__user = 'root'; 
+	private $__passw = ''; 
     private $__base = 'drive2-auto'; 
     private $__table = 'parser'; 
 	
-	public function Choice_Base_Table($base, $table){
+	public function Choice_Base($host, $user, $passw, $base, $table){
+		$this->__host = $host;
+		$this->__user = $user;
+		$this->__passw = $passw;
 		$this->__base = $base; 
 		$this->__table = $table; 
 	}
     
-    public function GetInfo() {
+    public function GetInfo($link_marks, $link_modells) {
 		
 		$ch = curl_init(); 
 		curl_setopt($ch, CURLOPT_URL, "example.com"); 
@@ -33,7 +91,7 @@ class Drive2_auto {
 
 		set_time_limit ( 3000 );
 
-		$link = mysqli_connect("localhost", "root", '', $this->__base);
+		$link = mysqli_connect($this->__host, $this->__user, $this->__passw, $this->__base);
 
 		$query = 'OPEN TABLE '.$this->__table.')';
 		mysqli_query($link, $query);
@@ -48,9 +106,9 @@ class Drive2_auto {
 		preg_match_all('#<a[^>]+?class\s*?=\s*?["\']c-link c-link--text["\'][^>]+?href=["\'](.+?)["\'][^>]*?>(.+?)</a>#su', $html_str, $marks);
 		$count_marks = count($marks[1]);
 
-		for ($key_mark = 2; $key_mark < $count_marks; ++$key_mark) {
-	
+		for (; $key_mark < $count_marks; ++$key_mark) {
 
+			if($link_marks == 'm' || $link_marks == $marks[1][$key_mark]){
 			$html_str_cars = file_get_contents("https://www.drive2.ru".$marks[1][$key_mark]);
 
 			$modells = array();
@@ -58,7 +116,8 @@ class Drive2_auto {
 			$count_modells = count($modells[1]);
 	
 			for($key_modell = 1; $key_modell < $count_modells; ++$key_modell){
-		
+				
+				if($link_modells == 'm' || $link_modells == $modells[1][$key_modell]){
 				curl_setopt($ch, CURLOPT_URL, "https://www.drive2.ru".$modells[1][$key_modell]); 
 				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 				$html_str_cars = curl_exec($ch); 
@@ -101,6 +160,8 @@ class Drive2_auto {
 					mysqli_query($link, $query);
 		
 				}
+				}
+			}
 			}
 		}
 
@@ -108,8 +169,13 @@ class Drive2_auto {
 		mysqli_close($link);
 
 			}
-    function GetBase() { 
-		$link = mysqli_connect("localhost", "root", '', "drive2-auto");
+    function GetBase($link_marks, $link_modells) { 
+		
+		if($link_marks != 'm')
+		$arr_marks = explode('/', $link_marks);
+		if($link_modells != 'm')
+		$arr_modells = explode('/', $link_modells);
+		$link = mysqli_connect($this->__host, $this->__user, $this->__passw, $this->__base);
 
 		$query = 'OPEN TABLE parser)';
 		mysqli_query($link, $query);
@@ -117,7 +183,11 @@ class Drive2_auto {
 		$result = mysqli_query($link, $query);
 		while (  $row  =  mysqli_fetch_row($result)  )
 		{
-		    print "<br>car_name: $row[0]<br>car_link: $row[1]<br>user_name: $row[2]<br>user_link: $row[3]<br>address: $row[4]<br>passport: $row[5]<br>";
+			if($link_marks == 'm' && $link_modells == 'm')
+				print "<br>car_name: $row[0]<br>car_link: $row[1]<br>user_name: $row[2]<br>user_link: $row[3]<br>address: $row[4]<br>passport: $row[5]<br>";
+			else
+			if(($link_marks != 'm' && mb_strstr($row[0], $arr_marks[1])) || ($link_modells != 'm' && mb_strstr($row[0], $arr_modells[2])))
+				print "<br>car_name: $row[0]<br>car_link: $row[1]<br>user_name: $row[2]<br>user_link: $row[3]<br>address: $row[4]<br>passport: $row[5]<br>";
 		};
 		mysqli_close($link);
     } 
